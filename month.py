9da7463240a82c6706939fd0acf87e2561cfb6ac
SerhiiH <begofnl@gmail.com>
2019-10-03 22:18:24 +0300

    Add month.py

---
+++ month.py
@@ -0,0 +1,17 @@
+from expenses import Expenses
+from incomes import Incomes
+from savings import Savings
+
+class Month:
+	def __init__(self, name):
+		self.__items = {'e': Expenses(), 'i': Incomes(), 's': Savings()}
+		self.__name = name
+		
+	def __repr__(self):
+		return self.__name.upper().center(30, ' ') + '\n' + str(self.__items['i']) + str(self.__items['e']) + str(self.__items['s']) 
+
+	def __getitem__(self, key):
+		return self.__items[key.lower()]
+
+		
+m = Month('October 2019')
