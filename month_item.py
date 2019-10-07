40196ac7c95665c46ff00d89e1c6bbb22f65843b
SerhiiH <begofnl@gmail.com>
2019-10-07 23:55:15 +0300

    month_item.py advanced

--- month_item.py
+++ month_item.py
@@ -3,24 +3,61 @@ from record import Record
 class MonthItem:
 	def __init__(self, name):
 		self.name = name
-		self.items = {}
+		self.itemsTypes = {}
 		self.itemsSources = {}
 		
 	def __repr__(self):
-		string = ''.join([str(rec) for rec in self.items.values()])
-		return self.name.capitalize().center(30, '-') + '\n' + string + '\n'
+		string = ''.join(['{0} (source: {1}):\n'.format(item.capitalize(), self.itemsSources[item].capitalize()) 
+				+ str(rec) for item, rec in self.itemsTypes.items()])
+		return self.name.capitalize().center(38, '-') + '\n' + string + '\n'
 		
-	def addItem(self, name, currencies = ['uah'], source = 'wallet'):
+	def addItem(self, name, *currencies, source = 'wallet'):
 		name = name.lower()
-		self.items[name] = Record(name, currencies)
-		self.itemsSources[name] = source.lower()
+		currencies = [curr.lower() for curr in currencies]
+		source = source.lower()
+		self.itemsTypes[name] = Record(name, currencies)
+		self.itemsSources[name] = source
 		
+	def deleteItem(self, item):
+		try:
+			del self.itemsTypes[item.lower()]
+			del self.itemsSources[item.lower()]
+		except KeyError:
+			print('ERROR!!! Incorrect Item name.')
+			return
 		
+	def changeItemAmount(self, item, currency, amount, sign = 1):
+		try:
+			self.itemsTypes[item.lower()].changeAmount(currency.lower(), float(amount), sign)
+		except KeyError:
+			print('ERROR!!! Incorrect Item name.')
+			return
+		except ValueError:
+			print('ERROR!!! Incorrect amount format.')
+			return
+	
+	def getItemSource(self, item):
+		return self.itemsSources[item.lower()]
 		
 		
 if __name__ == '__main__':
 	mi = MonthItem('Expenses')
-	mi.addItem('Rent', ['uah'])
-	mi.addItem('TRAVELLING', ['uah', 'usd', 'eur'])
-	mi.addItem('FooD', ['uah',])
+	mi.addItem('Rent', 'uah')
+	mi.addItem('TRAVELLING', 'uah', 'usd', 'eur', source = 'travelling')
+	mi.addItem('FooD', 'uah')
 	print(mi)
+	
+	mi.changeItemAmount('rent', 'uah', 1000)
+	print(mi)
+	mi.changeItemAmount('rent', 'uah', 500, -1)
+	print(mi)
+	mi.changeItemAmount('rent', 'usd', 500)
+	print(mi)
+	mi.changeItemAmount('bikes', 'uah', 500)
+	print(mi)
+	mi.changeItemAmount('rent', 'uah', '541-')
+	print(mi)
+	
+	mi.deleteItem('rent')
+	print(mi)
+	
--- record.py
+++ record.py
@@ -1,21 +1,22 @@
 class Record:
 	def __init__(self, name, currencies):
 		self.name = name
-		self.amount = {currency.lower(): 0 for currency in currencies}
+		self.amount = {currency: 0 for currency in currencies}
 		
 	def __repr__(self):
-		string = ''.join(['{0:>25}: {1:,}\n'.format(curr.upper(), val) for curr, val in self.amount.items()])
-		return self.name.capitalize() + ':\n' + string
+		return ''.join(['{0:>35}: {1:,}\n'.format(curr.upper(), val) for curr, val in self.amount.items()])
 		
 	def changeAmount(self, currency, amount, sign = 1):
-		currency = currency.lower()
-		self.amount[currency] += amount * sign
+		try:
+			self.amount[currency] += amount * sign
+		except KeyError:
+			print('ERROR!!! Incorrect currency.')
 
 if __name__ == '__main__':
 	r = Record('rent', ['uah', 'usd', 'eur'])
 	print(r)
-	r.changeAmount('UsD', 200)
+	r.changeAmount('usd', 200)
 	print(r)
-	r.changeAmount('uSd', 70, -1)
+	r.changeAmount('usd', 70, -1)
 	print(r)
 	
