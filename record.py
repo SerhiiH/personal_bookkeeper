5f9e08ca0fcac04d91d8a6d67ff070ad9f55627e
SerhiiH <begofnl@gmail.com>
2019-10-07 23:11:34 +0300

    month_item.py basic

--- record.py
+++ record.py
@@ -1,12 +1,11 @@
 class Record:
-	def __init__(self, name, currencies = ['uah'], source = 'wallet'):
-		self.name = name.lower()
+	def __init__(self, name, currencies):
+		self.name = name
 		self.amount = {currency.lower(): 0 for currency in currencies}
-		self.source = source.lower()
 		
 	def __repr__(self):
 		string = ''.join(['{0:>25}: {1:,}\n'.format(curr.upper(), val) for curr, val in self.amount.items()])
-		return self.name.capitalize() + '(source: {0}):\n'.format(self.source) + string
+		return self.name.capitalize() + ':\n' + string
 		
 	def changeAmount(self, currency, amount, sign = 1):
 		currency = currency.lower()
@@ -15,7 +14,7 @@ class Record:
 if __name__ == '__main__':
 	r = Record('rent', ['uah', 'usd', 'eur'])
 	print(r)
-	r.changeAmount('UsDs', 200)
+	r.changeAmount('UsD', 200)
 	print(r)
 	r.changeAmount('uSd', 70, -1)
 	print(r)
